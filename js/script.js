a3441f7f47d3ee2286f02b6558dfff5cf2125804
nikita <nikitakremnev0@gmail.com>
2022-02-15 21:59:12 +0300

    Final

--- js/script.js
+++ js/script.js
@@ -10,4 +10,12 @@ const personalMovie = {
     genres:[],
     private: false
 };
+const lastFilm = prompt("последний фильм,который вы посмотрели");
+const gradeOfFilms = prompt("оцените его по 10 бальной шкале");
 
+personalMovie.movies={
+        nameOfFilms: lastFilm,
+        grade: gradeOfFilms
+    };
+
+console.log(`${personalMovie.movies.nameOfFilms}:`,`${personalMovie.movies.grade}`);
