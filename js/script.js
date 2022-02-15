610beccf89e93e1e65e20213e1b1c87af59cf459
nikita <nikitakremnev0@gmail.com>
2022-02-15 21:41:07 +0300

    get Object

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 
 const namberOfFilms = prompt("сколько фильмов вы посмотрели?");
 console.log(namberOfFilms);
+
+const personalMovie = {
+    count: namberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    private: false
+};
+
