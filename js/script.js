64765a0904849cde654c870ce063951727775fd2
Кремнев Никита <webmaster@farexpo.local>
2022-04-06 16:07:17 +0300

    work2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const namberOfFilms = +prompt("сколько фильмов вы посмотрели?", "");
+/* const namberOfFilms = +prompt("сколько фильмов вы посмотрели?", "");
 
 if(namberOfFilms > 50) {
     console.log('вы киноман!');
@@ -22,17 +22,25 @@ const personalMovie = {
 
 for (let i = 0; i < 2; i++){
 
-let a = prompt("последний фильм,который вы посмотрели", ""),
-    b = prompt("оцените его по 10 бальной шкале", "");    
+    let a = prompt("последний фильм,который вы посмотрели", ""),
+        b = prompt("оцените его по 10 бальной шкале", "");    
 
-if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-  personalMovie.movies[a] = b;   
- console.log(personalMovie.movies[a] + ': ' + personalMovie.movies[b]);    
-} else{
-i--
-}   
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovie.movies[a] = b;      
+    } else{
+        i--;
+    }   
 }
 
 
 console.log(personalMovie);
+
+ */
+
+function fi (i, a) {
+    
+    return (i + a);
+}
+
+console.log(fi(4, 22));
 
