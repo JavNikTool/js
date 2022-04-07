6ae2aacbd61888d069fbc97ebf55130daeca0988
Кремнев Никита <webmaster@farexpo.local>
2022-04-07 14:48:27 +0300

    work3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 "use strict";
+let namberOfFilms;
 
-/* const namberOfFilms = +prompt("сколько фильмов вы посмотрели?", "");
 
-if(namberOfFilms > 50) {
-    console.log('вы киноман!');
-} else if (namberOfFilms < 50) {
-    console.log('просмотрено слишком мало фильмов');
-}else{
-    console.log('произошла ошибка');
+function start () {
+    namberOfFilms = +prompt("сколько фильмов вы посмотрели?", "");
+    while(namberOfFilms == '' || namberOfFilms == null || isNaN(namberOfFilms)){
+        namberOfFilms = +prompt("сколько фильмов вы посмотрели?", "");
+    }
 }
+
+start(); 
+
+function countingOfFilms(){
+    if(namberOfFilms > 50) {
+        console.log('вы киноман!');
+    } else if (namberOfFilms < 50) {
+        console.log('просмотрено слишком мало фильмов');
+    }else{
+        console.log('произошла ошибка');
+    }
+}
+
+countingOfFilms();
 
 const personalMovie = {
     count: namberOfFilms,
@@ -18,29 +31,55 @@ const personalMovie = {
     private: false
 };
 
+function writeYourGentes(){
+    for (let i = 1; i < 4; i++){
 
+        let c = prompt(`ваш любимый жанр под номером ${i}`);
 
-for (let i = 0; i < 2; i++){
+        if (c != null && c != '' && c.length < 15) {
+        personalMovie.genres[i - 1] = c;      
+        } else{
+            i--;
+        }   
+    }    
+}
+writeYourGentes();
 
-    let a = prompt("последний фильм,который вы посмотрели", ""),
-        b = prompt("оцените его по 10 бальной шкале", "");    
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-    personalMovie.movies[a] = b;      
-    } else{
-        i--;
-    }   
+function showDB(){
+    if(personalMovie.private == false){
+        console.log(personalMovie);
+    }
 }
 
+function rememver() {
+    for (let i = 0; i < 2; i++){
+
+        let a = prompt("последний фильм,который вы посмотрели", ""),
+            b = prompt("оцените его по 10 бальной шкале", "");    
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovie.movies[a] = b;      
+        } else{
+            i--;
+        }   
+    }    
+}
+
+rememver();
 
 console.log(personalMovie);
 
- */
+ 
 
-function fi (i, a) {
+/*  function fi (i, a) {
     
     return (i + a);
 }
 
 console.log(fi(4, 22));
+
+const c = (a,b) => a + b;
+
+console.log(c(4,7));  */
+
 
