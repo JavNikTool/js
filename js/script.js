6dab75acbeb9902d9b599beeb01675a71d6d047b
Кремнев Никита <webmaster@farexpo.local>
2022-06-23 15:12:22 +0300

    arrTest

--- js/script.js
+++ js/script.js
@@ -1,85 +1,48 @@
 "use strict";
 
-/* function countingOfFilms(){
-    
-}
 
-countingOfFilms(); */
 
-const personalMovie = {
-    count: 0,
-    movies:{},
-    actors:{},
-    genres:[],
-    private: true,
-    start: function() {
-        personalMovie.count = +prompt("сколько фильмов вы посмотрели?", "");
-        while(personalMovie.count == '' || personalMovie.count == null || isNaN(personalMovie.count)){
-            personalMovie.count = +prompt("сколько фильмов вы посмотрели?", "");
-        }
-        if(personalMovie.count > 50) {
-            console.log('вы киноман!');
-        } else if (personalMovie.count < 50) {
-            console.log('просмотрено слишком мало фильмов');
+
+const box = document.querySelector('.box');
+box.addEventListener('click', () =>  {
+box.classList.toggle('test');
+});
+
+
+let arr = ['nikita', 'sveta', 'katya', 'Gena', 1, 44, 'aleksey', -13, 441, - 3, {name: 'Кирилл', age: 25}, 42];
+
+/* arr.forEach(i => console.log(i)); */
+
+/* const nerArr = arr.map(element => {
+    return element;
+});
+console.log(nerArr); */
+let newArr = arr.filter(element => element <= 0 && typeof(element) != 'string');
+console.log(newArr);
+
+let arr2 = [131,1,43,5,476,245,9,5,134,345,444,23];
+let max = 0;
+let b = arr2.reduce( (value, item) => {
+        if(value > max){
+            max = value;
         }else{
-            console.log('произошла ошибка');
+            return item;
         }
-    },
-    writeYourGentes: function(){
-        for (let i = 1; i < 4; i++){
-            let c = prompt(`ваш любимый жанр под номером ${i}`);
-            if (c != null && c != '' && c.length < 15) {
-            personalMovie.genres[i - 1] = c;      
-            } else{
-                i--;
-            }   
-        }
-        personalMovie.genres.forEach((item, i) => {
-            console.log(`Любимый жанр под номером ${i+1}: ${item}`);
-        });
-    },
-    rememver: function() {
-        for (let i = 0; i < 2; i++){
-    
-            let a = prompt("последний фильм,который вы посмотрели", ""),
-                b = prompt("оцените его по 10 бальной шкале", "");    
-    
-            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovie.movies[a] = b;      
-            } else{
-                i--;
-            }   
-        }    
-    },
-    showDB: function(){
-        if(personalMovie.private == false){
-            console.log(personalMovie);
-        }
-    },
-    toggleVisibleDB: function() {
-        if(personalMovie.private){
-            personalMovie.private = false;
-        }else if(personalMovie.privat){
-            personalMovie.private = true;
-        }
-    }
-};
-personalMovie.start();
-personalMovie.writeYourGentes();
-personalMovie.rememver();
-personalMovie.toggleVisibleDB();
-personalMovie.showDB();
+},0)
+
+console.log(max);
+
+
+let arr3 = [414, -134, 43 ,12 ,52 ,63 ,-14 ,-341 ,-54 ,134 ,-4];
+
+let new2 = arr3.map((elem, i) => {
+   return elem = `значение элемента под номером ${i} равно ${elem}`;
+});
+box.append(arr3);
+console.log(new2);
 
 
 
+/* let nnaarr = arr3.filter(element => element <= 0);
 
-/*  function fi (i, a) {
-    
-    return (i + a);
-}
-
-console.log(fi(4, 22));
-
-const c = (a,b) => a + b;
-
-console.log(c(4,7));  */
+console.log(nnaarr); */
